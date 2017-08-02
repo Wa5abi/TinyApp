693c6124b92b08b98292a79410736b9e61c383ca
Dene <W4548147gm411@gmail.com>
2017-08-02 22:30:54 +0000

    added form to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -38,8 +38,10 @@ function generateRandomString() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 6; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
 }
+
+console.log(generateRandomString())
