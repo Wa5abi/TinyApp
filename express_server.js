3f94d29a3b57d9eec518c454ffa10830970c2a3b
Dene <W4548147gm411@gmail.com>
2017-08-02 22:10:12 +0000

    adds the generateRandomString function to express_sever.js

--- express_server.js
+++ express_server.js
@@ -11,21 +11,35 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-function requestHandler (req, res) {
-  app.get("/", (req, res) => {
-    res.end("Hello!");
-  });
 
-  app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
 
-  app.get("/hello", (req, res) => {
-    res.end("<html><body>Hello <b>World</b></body></html>\n");
-  });
-}
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
